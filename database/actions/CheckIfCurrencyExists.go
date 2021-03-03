548665b8c5c65f8cb8024c4c76f0dce9bb273477
MathisBurger <handymathis@gmail.com>
2021-03-03 12:04:26 +0100

    commented CheckIfCurrencyExists.go

--- database/actions/CheckIfCurrencyExists.go
+++ database/actions/CheckIfCurrencyExists.go
@@ -1,17 +1,16 @@
 package actions
 
 func CheckIfCurrencyExists(name string) bool {
+
 	conn, _ := connect()
 	defer conn.Close()
-	stmt, err := conn.Prepare("SELECT * FROM `currencys` WHERE `symbol`=?")
-	if err != nil {
-		panic(err.Error())
-	}
+
+	stmt, _ := conn.Prepare("SELECT * FROM `currencys` WHERE `symbol`=?")
 	defer stmt.Close()
-	resp, err := stmt.Query(name)
-	if err != nil {
-		panic(err.Error())
-	}
+
+	resp, _ := stmt.Query(name)
 	defer resp.Close()
+
+	// exists if true
 	return resp.Next()
 }
