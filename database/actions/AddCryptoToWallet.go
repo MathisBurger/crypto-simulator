3e7c41b91e854f4a8763d6942bceff4c3732d79b
MathisBurger <handymathis@gmail.com>
2021-03-03 12:01:06 +0100

    commented AddCryptoToWallet.go

--- database/actions/AddCryptoToWallet.go
+++ database/actions/AddCryptoToWallet.go
@@ -8,15 +8,23 @@ import (
 )
 
 func AddCryptoToWallet(UUID string, symbol string, amount float64) {
+
 	conn, _ := connect()
 	defer conn.Close()
+
 	stmt, _ := conn.Prepare("SELECT * FROM `wallets` WHERE `UUID`=?")
 	defer stmt.Close()
+
 	resp, _ := stmt.Query(UUID)
 	defer resp.Close()
+
 	resp.Next()
+
+	// get array of user currencies
 	currencyArray := strings.Split(models.WalletModel{}.Parse(resp).CurrencyArray, ";")
 	var builder strings.Builder
+
+	// get currency from array and increase it
 	if len(currencyArray) != 0 {
 		found := false
 		for i, el := range currencyArray {
@@ -27,9 +35,13 @@ func AddCryptoToWallet(UUID string, symbol string, amount float64) {
 				found = true
 			}
 		}
+
+		// if array does not contain add it
 		if !found {
 			currencyArray = append(currencyArray, symbol+"|"+fmt.Sprintf("%f", amount))
 		}
+
+		// build currencyArray to string
 		for i, el := range currencyArray {
 			if i == 0 {
 				builder.WriteString(el)
@@ -37,10 +49,13 @@ func AddCryptoToWallet(UUID string, symbol string, amount float64) {
 			}
 			builder.WriteString(";" + el)
 		}
+
 	} else {
 		builder.WriteString(symbol + "|" + fmt.Sprintf("%f", amount))
 	}
+
 	stmt, _ = conn.Prepare("UPDATE `wallets` SET `currencyArray`=? WHERE `UUID`=?")
 	defer stmt.Close()
+
 	stmt.Exec(builder.String(), UUID)
 }
