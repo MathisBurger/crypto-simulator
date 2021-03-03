73e95aa8930e6eb9164ed9d37388f1c585b5d508
MathisBurger <handymathis@gmail.com>
2021-03-03 11:47:45 +0100

    commented GetCryptoWalletsForUser.go

--- controller/GetCryptoWalletsForUser.go
+++ controller/GetCryptoWalletsForUser.go
@@ -7,14 +7,17 @@ import (
 )
 
 type getCryptoWalletsForUserResponse struct {
-	Status bool `json:"status"`
-	Message string `json:"message"`
-	Data []models.CurrencyArrayModel `json:"data"`
+	Status  bool                        `json:"status"`
+	Message string                      `json:"message"`
+	Data    []models.CurrencyArrayModel `json:"data"`
 }
 
 func GetCryptoWalletsForUser(c *fiber.Ctx) error {
+
 	username := c.Query("username")
 	token := c.Query("token")
+
+	// check default values
 	if username == "" || token == "" {
 		return c.JSON(getCryptoWalletsForUserResponse{
 			false,
@@ -22,12 +25,15 @@ func GetCryptoWalletsForUser(c *fiber.Ctx) error {
 			nil,
 		})
 	}
+
+	// check login
 	if actions.LoginWithToken(username, token) {
 		return c.JSON(getCryptoWalletsForUserResponse{
 			true,
 			"successfully queried all trades from the last 7 days",
 			actions.GetCurrencyArray(actions.GetUserByUsername(username).WalletUUID),
 		})
+
 	} else {
 		return c.JSON(getCryptoWalletsForUserResponse{
 			false,
