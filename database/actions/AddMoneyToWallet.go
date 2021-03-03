af5c747c9d9ac86c1ff65ba5313890c73a554330
MathisBurger <handymathis@gmail.com>
2021-03-03 12:02:35 +0100

    commented AddMoneyToWallet.go

--- database/actions/AddMoneyToWallet.go
+++ database/actions/AddMoneyToWallet.go
@@ -3,16 +3,27 @@ package actions
 import "github.com/MathisBurger/crypto-simulator/database/models"
 
 func AddMoneyToWallet(UUID string, amount float64) {
+
 	conn, _ := connect()
 	defer conn.Close()
+
 	stmt, _ := conn.Prepare("SELECT * FROM `wallets` WHERE `UUID`=?")
 	defer stmt.Close()
+
 	resp, _ := stmt.Query(UUID)
 	defer resp.Close()
+
 	resp.Next()
+
+	// get wallet model
 	mdl := models.WalletModel{}.Parse(resp)
+
+	// calculate new value
 	newValue := mdl.BalanceUSD + amount
+
+	// update balance
 	stmt, _ = conn.Prepare("UPDATE `wallets` SET `balanceUSD`=? WHERE `UUID`=?")
 	defer stmt.Close()
+
 	stmt.Exec(newValue, UUID)
 }
