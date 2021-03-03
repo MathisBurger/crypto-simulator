fa6cc354d93f09db73f7fa3b193bb89b1e5d34e5
MathisBurger <handymathis@gmail.com>
2021-03-03 11:55:13 +0100

    commented SellCryptoController.go

--- controller/SellCryptoController.go
+++ controller/SellCryptoController.go
@@ -19,9 +19,10 @@ type sellCryptoResponse struct {
 }
 
 func SellCryptoController(c *fiber.Ctx) error {
-	raw := string(c.Body())
+
+	// parsing and checking request
 	obj := sellCryptoRequest{}
-	err := json.Unmarshal([]byte(raw), &obj)
+	err := json.Unmarshal(c.Body(), &obj)
 	if err != nil {
 		return c.JSON(sellCryptoResponse{
 			false,
@@ -34,29 +35,41 @@ func SellCryptoController(c *fiber.Ctx) error {
 			"Invalid JSON body",
 		})
 	}
+
+	// must be minimum 0
 	if obj.Amount <= 0 {
 		return c.JSON(sellCryptoResponse{
 			false,
 			"Value must be higher than zero",
 		})
 	}
+
+	// check login
 	if actions.LoginWithToken(obj.Username, obj.Token) {
+
 		user := actions.GetUserByUsername(obj.Username)
 		currency := actions.GetCurrency(obj.CurrencyID)
+
+		// check if user own given number of crypto
 		if actions.GetAmountOfCryptoByUUID(user.WalletUUID, currency.Symbol) >= obj.Amount {
+
+			// sell crypto
 			actions.RemoveCryptoFromWallet(user.WalletUUID, currency.Symbol, obj.Amount)
 			actions.AddMoneyToWallet(user.WalletUUID, currency.PriceUSD*obj.Amount)
 			actions.AddTrade(currency.Symbol, "USD", user.WalletUUID, currency.PriceUSD, obj.Amount)
+
 			return c.JSON(sellCryptoResponse{
 				true,
 				"Successfully sold crypto currency",
 			})
+
 		} else {
 			return c.JSON(sellCryptoResponse{
 				false,
 				"You do not have enough crypto in your Wallet",
 			})
 		}
+
 	} else {
 		return c.JSON(sellCryptoResponse{
 			false,
@@ -65,6 +78,7 @@ func SellCryptoController(c *fiber.Ctx) error {
 	}
 }
 
+// checks request
 func checkSellCryptoRequest(obj sellCryptoRequest) bool {
 	return obj.Username != "" && obj.Token != "" && obj.CurrencyID != ""
 }
